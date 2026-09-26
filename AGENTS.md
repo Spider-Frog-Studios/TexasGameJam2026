ee3ead0d02ddd021539b56a416ae2d126f11d118
Voltspider <76660439+Iornspider1@users.noreply.github.com>
2026-09-25 23:14:34 -0500

    Added MouseGrab
    Allows the user to grab windows by the GrabBar up at the top of each of them.

--- AGENTS.md
+++ AGENTS.md
@@ -2,7 +2,7 @@
 - Project name: TexasGameJam2026
 - Unity version: Unity 6000.4.7f1
 - Active game object:
-  - Name: Window
+  - Name: InputManager
   - Tag: Untagged
   - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
---
+++ Assets/Scripts/DesktopCursorController.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DesktopCursorController : MonoBehaviour
+{
+
+    private Camera mainCamera;
+    private GameObject selectedWindow;
+    private bool leftMousePressed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        mainCamera = Camera.main;
+        leftMousePressed = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowMouse();
+
+        if (leftMousePressed)
+        {
+            Vector2 mouseScreenPos = Mouse.current.position.ReadValue();
+            Vector2 mouseWorldPos = mainCamera.ScreenToWorldPoint(mouseScreenPos);
+
+            Collider2D hitCollider = Physics2D.OverlapPoint(mouseWorldPos);
+
+            if (hitCollider != null)
+            {
+                GameObject clickedObject = hitCollider.gameObject;
+
+                if (clickedObject.GetComponent<MouseGrab>())
+                {
+                    selectedWindow = clickedObject;
+                    Debug.Log("Window clicked!");
+                    selectedWindow.GetComponent<MouseGrab>().StartDragging(this.transform);
+                } else
+                {
+                    Debug.Log("Clicked object is not a window.");
+                }
+            } else
+            {
+                Debug.Log("No object clicked.");
+            }
+        } else if(selectedWindow != null)
+        {
+            selectedWindow.GetComponent<MouseGrab>().StopDragging();
+            selectedWindow = null;
+        }
+    }
+
+    void OnClick(InputValue value)
+    {
+        leftMousePressed = value.isPressed;
+    }
+
+    private void FollowMouse()
+    {
+        Vector3 targetPos = mainCamera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        targetPos.z = 0;
+        transform.position = targetPos;
+    }
+
+
+}
